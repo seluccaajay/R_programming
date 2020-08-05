df63aecab050a3e82d9f600fd428aea17cd91f74
Ajaykrishnan Selucca <56751125+seluccaajay@users.noreply.github.com>
2020-08-05 21:50:01 +0530

    Add files via upload
    Wrote test on section 1 of edx.

---
+++ 03_05082020/Test.R
@@ -0,0 +1,19 @@
+# To find the solutions to an equation of the format  ax2+bx+c , use the quadratic equation:  x=???b±b2???4ac???2a .
+# 
+# What are the two solutions to  2x2???x???4=0  ? Use the quadratic equation.
+# Report the positive solution first, using 3 significant digits for both solutions (both will be numbers).
+
+a <- 2
+b <- -1
+c <- -4
+
+x <- (-b + sqrt(b^b - 4*a*c))/(2*a)
+
+y <- (-b - sqrt(b^b - 4*a*c))/(2*a)
+
+print(x)
+
+print(y)
+
+log(x=1024,base=4)
+
---
+++ 03_05082020/test2.R
@@ -0,0 +1,12 @@
+library(dslabs)
+data(movielens)
+
+names(movielens)
+
+length(movielens$year)
+
+class(movielens$title)
+
+class(movielens$genres)
+
+nlevels(movielens$genres)
